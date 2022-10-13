ccfa97e23265cdc4c1bf3792c9c68185c7c75cf9
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-10-13 14:29:00 -0400

    commit tree 47795bb7be0f23da7484e121bf35c3f45a093460

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    #byebug
+    if params[:name]==nil
+      students = Student.all 
+    else 
+      name = params[:name].titleize
+      students = Student.where(first_name: name).or(Student.where(last_name: name))
+    end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
